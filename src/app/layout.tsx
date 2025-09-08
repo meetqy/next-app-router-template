ad7f229a19b57582a8471e92cd806fa3414ed602
shirlyyang <shirlyyang@tencent.com>
2025-09-08 17:12:59 +0800

    favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/eo-logo-blue.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
